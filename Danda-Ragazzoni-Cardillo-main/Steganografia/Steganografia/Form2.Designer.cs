20048865a1652f3a3ed758ea624a721861432f80
R3gaz <35299739+R3gaz@users.noreply.github.com>
2021-03-17 10:18:52 +0100

    Add files via upload

---
+++ Danda-Ragazzoni-Cardillo-main/Steganografia/Steganografia/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Steganografia
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Carica Immagine da modificare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(229, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 270);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salva Immagine";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 141);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(185, 137);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Scrivi il messaggio da nascondere: ";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Danda-Ragazzoni-Cardillo-main/Steganografia/Steganografia/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganografia
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)      //caricamento immagine
+        {
+            OpenFileDialog apri = new OpenFileDialog();     //variabile di tipo "carica file"
+            apri.Filter = "File immagine(*.jpg; *.jpeg; *.gif; *.png; *.bmp) | *.jpg; *.jpeg; *.gif; *.png; *.bmp";     //filtro per poter inserire solo immagini
+            if (apri.ShowDialog() == DialogResult.OK)       //se il caricamento del file avviene con successo
+            {
+                pictureBox1.Image = new Bitmap(apri.FileName);      //metto l'immagine caricata nella picturebox
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                MessageBox.Show("Non hai caricato un'immagine!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                if (richTextBox1.Text.Length == 0)
+                    MessageBox.Show("Non hai inserito il messaggio!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                else
+                {
+                    pictureBox1.Image = steganografia(pictureBox1.Image, richTextBox1.Text);
+                }
+            }
+        }
+        private Image steganografia(Image input, string messaggio)
+        {
+            int x = 0, y = 0;
+            int red = 0, green = 0, blue = 0;
+            Bitmap output = new Bitmap(input.Width, input.Height);
+
+            //output.SetPixel(x, y, colore);
+            return output;
+        }
+    }
+}
